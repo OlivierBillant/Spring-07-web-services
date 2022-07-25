7c8d2cb7b4a107034dcf3111bdd1c63bc2bb4da2
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-25 14:04:10 +0200

    CREATE

---
+++ src/main/java/com/example/Spring07webservice/Spring07WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring07webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07WebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring07webservice/Spring07WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring07webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
